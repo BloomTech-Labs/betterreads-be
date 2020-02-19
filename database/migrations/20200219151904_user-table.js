a48ae67b349694008cd052cccf6d9f74119c12fc
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-19 15:30:52 -0800

    added user-table

--- database/migrations/20200219151904_user-table.js
+++ database/migrations/20200219151904_user-table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+	return knex.schema
+		.createTable('users', tbl => {
+			tbl.increments();
+			tbl.string('email', 50)
+				 .notNullable()
+				 .unique();
+			tbl.string('password', 250)
+				 .notNullable();
 };
 
 exports.down = function(knex) {
-  
+	return knex.schema.dropTableIfExists('users');
 };
