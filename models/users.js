126da1057d1193c7f6ef7634cb6ba103026d7249
Aasa-Christian <aasachristian@yahoo.com>
2020-02-19 18:56:08 -0500

    added user model for login

---
+++ models/users.js
@@ -0,0 +1,23 @@
+const db = require('../database/db-config.js');
+
+module.exports = {
+  findBy,
+  add,
+  findById
+};
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
