12332cde0c11e10d291c55d78726b5d28dd8b4b5
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-21 14:53:57 -0800

    added seeds to users and books. modified book-router to show status codes. and created book-router.test.js

--- routers/book-router.js
+++ routers/book-router.js
@@ -8,7 +8,7 @@ router.get('/:bookId', (req, res) => {
     const bookId = req.params.bookId 
     books.findById(bookId)
       .then(book => {
-        res.json(book);
+        res.status(200).json(book);
       })
       .catch(err => res.status(500).json({message: 'book not found'}));
   });
