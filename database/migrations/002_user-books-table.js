c85b046bd0921f38066b67abebe38a4f2870976f
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-21 12:41:38 -0800

    added books table amd user-books table and renamed users table. Added empty file books.js to model

---
+++ database/migrations/002_user-books-table.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+	return knex.schema
+		.createTable('userBooks', tbl => {
+			tbl.increments();
+			tbl.integer('bookId')
+			   .unsigned()
+			   .notNullable()
+			   .references('id')
+			   .inTable('books')
+			   .onDelete('CASCADE')
+			   .onUpdate('CASCADE');
+
+			tbl.integer('readingStatus')
+				.notNullable()
+				.defaultTo(1);
+
+			tbl.integer('userId')
+			   .unsigned()
+			   .notNullable()
+			   .references('id')
+			   .inTable('users')
+			   .onDelete('CASCADE')
+			   .onUpdate('CASCADE');
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTableIfExists('userBooks');
+};
