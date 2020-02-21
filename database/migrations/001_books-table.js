d150b37f22c6cf34dd5cc06ee3d274abc125b931
Aasa-Christian <aasachristian@yahoo.com>
2020-02-21 16:18:14 -0500

    created user-shelf-table, user-books-on-a-shelf-table

--- database/migrations/001_books-table.js
+++ database/migrations/001_books-table.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
 		return knex.schema
 		.createTable('books', tbl => {
 			tbl.increments();
-			tbk.string('googleId').notNullable();
+			tbl.string('googleId').notNullable();
 			tbl.string('title').notNullable();
 			tbl.string('author').notNullable();
 			tbl.string('publisher').notNullable();
@@ -13,13 +13,13 @@ exports.up = function(knex) {
 			tbl.string('isbn13');
 			tbl.integer('pageCount').notNullable();
 			tbl.string('categories').notNullable();
-			tbl.string('thumbnail')notNullable();
+			tbl.string('thumbnail').notNullable();
 			tbl.string('smallThumbnail').notNullable();
 			tbl.string('language').notNullable();
 			tbl.string('webRenderLink').notNullable();
 			tbl.string('textSnippet').notNullable();
 			tbl.boolean('isEbook').notNullable();
-};
+})};
 
 exports.down = function(knex) {
 	return knex.schema.dropTableIfExists('books');
