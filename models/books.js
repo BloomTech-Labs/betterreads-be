d279fd7c1184d3a221fe114ebb3d88f2014f9e26
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-21 16:09:39 -0800

    books-router add function to look through books table before adding to books. book-router.test.js has 2 failing tests. Fix

--- models/books.js
+++ models/books.js
@@ -10,14 +10,14 @@ function findBy(filter) {
     return db('books').where(filter);
   }
   
-  async function add(book) {
-    const [id] = await db('books').insert(book).returning('id');
-    return findById(id);
-  }
+async function add(book) {
+  const [id] = await db('books').insert(book).returning('id');
+  return findById(id);
+}
   
-  function findById(id) {
-    return db('books')
-      .where({ id })
-      .first();
-  }
+function findById(id) {
+  return db('books')
+    .where({ id })
+    .first();
+}
 
--- routers/book-router.js
+++ routers/book-router.js
@@ -1,26 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+const Books = require("../models/books");
 
-const books = require('../models/books');
+router.get("/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+  Books.findById(bookId)
+    .then(book =>
+      book == undefined
+        ? res.status(404).json({ message: "No books here" })
+        : res.status(200).json(book)
+    )
+    .catch(err => res.status(500).json({ message: "book not found" }));
+});
 
-
-router.get('/:bookId', (req, res) => {
-    const bookId = req.params.bookId 
-    books.findById(bookId)
-      .then(book => {
-        res.status(200).json(book);
-      })
-      .catch(err => res.status(500).json({message: 'book not found'}));
-  });
-
-router.post('/', (req, res) => {
-    const book = req.body.book
-    books.add(book)
-      .then(book => {
-        res.status(201).json(book);
-      })
-      .catch(err => res.status(500).json({message: 'book not added'}));
-  });
-  
+router.post("/", (req, res) => {
+  const book = req.body;
+  if (book) {
+    const title = book.title
+    Books.findBy({ title }).first().then(book => {
+      res.status(200).json(book);
+    })
+    .catch(err => {
+      Books.add(book)
+         .then(book => res.status(201).json({ message: "Could not find book in our api but added it", book: book }) )
+         .catch(err => res.status(500).json({ message: "book not added" }));
+    }) 
+  } else {
+      res.status(400).json({ message: 'please provide a book' });
+  }
+});
 
 module.exports = router;
