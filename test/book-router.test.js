9ae1a0fa3721158400b57844496ff02dd9b88648
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-21 16:09:39 -0800

    books-router add function to look through books table before adding to books. book-router.test.js has 2 failing tests. Fix

--- test/book-router.test.js
+++ test/book-router.test.js
@@ -3,6 +3,26 @@ const request = require("supertest");
 const db = require("../database/db-config.js");
 
 describe("book-router", function() {
+
+	const bookObject = {
+                googleId: "qwoldmcdfiom123103",
+                title: "Chantra Swandie",
+                author: "McWorld",
+                publisher:"Penguin",
+                publishDate:"2/21/2020",
+                description: "The end of the book",
+                isbn10: "12345678911234567891",
+                isbn13:"12345678911234567891234",
+                pageCount: 210,
+                categories:"swenad",
+                thumbnail:"image.png",
+                smallThumbnail:"small-img.png",
+                language: "english",
+                webRenderLink: "testLink",
+                textSnippet: "testSnippet",
+                isEbook: true
+	}
+
 	let token;
 	beforeEach(async function() {
 		await db("users").truncate();
@@ -18,6 +38,8 @@ describe("book-router", function() {
 	});
 
 	describe("GET api/books/1", function() {
+
+		// MARK: -- FIX NOT WORKING
 		it("GET book success status", function() {
 			return request(server)
 				.get("/api/books/1").set('authorization', token)
@@ -31,5 +53,34 @@ describe("book-router", function() {
 					expect(res.type).toMatch(/json/i);
 				})
 		});
+
+		it("Expect 401 with no authentication set in header", function() {
+			return request(server)
+				.get("/api/books/1")
+				.then(res => {
+					expect(res.status).toBe(401);
+				})
+		});
+
+		it("Expect error message for book not in database", function() {
+			return request(server)
+				.get("/api/books/2000000000").set('authorization', token)
+				.then(res => {
+					expect(res.body.message).toBe("No books here");
+				})
+		});
+	});
+
+	describe('POST a book', function() {
+		// MARK: -- fix not working
+		it('Expect a 201', function() {
+			return request(server)
+				.post('/api/books').set('authorization', token)
+				.send(bookObject)
+				.then(res => {
+					expect(res.status).toBe(201)
+				});
+		});
+
 	});
 });
