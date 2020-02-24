b5a7a2edf08b491fded9ba0429a910579f0e027f
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-24 15:46:24 -0800

    added post to user-books-router

--- routers/user-books-router.js
+++ routers/user-books-router.js
@@ -17,27 +17,49 @@ router.get('/:userId/library', (req, res) => {
 		);
 });
 
-// router.post('/:userId/library/', (req, res) => {
-// 	const userId = req.params.userId;
-// 	const book = req.body;
-//  Books.findBy({ title }).then(book => {
-//		if(book == undefined) {
-
-//		}
-//	}).catch()
-// 	const userbookObject = { bookId: bookId, readingStatus: , userId: userId}
-// 	UserBooks.add(book).then(added => {
-// 		console.log(added)
-// 		if(added == undefined) {
-// 			res.status(400).json({ message: 'userbooks: please provide book' });
-// 		} else {
-// 			res.status(201).json(added)
-// 		}
-// 	})
-// 	.catch(err => {
-// 		res.status(500).json({ message: 'error in posting userbook' });
-// 	})
-// })
+// MARK: -- REFACTOR
+router.post('/:userId/library/', (req, res) => {
+	const userId = req.params.userId;
+	const book = req.body.book;
+	const status = req.body.readingStatus;
+ 	if (book) {
+		const title = book.title;
+		Books.findBy({ title })
+			.first()
+			.then(bk => {
+				if (bk == undefined) {
+					Books.add(book)
+						.then(book => {
+							console.log(book)
+							const userbookObj = { bookId: book.id, readingStatus: status, userId: userId }
+							UserBooks.add(userbookObj)
+								.then(added => {
+									if(added == undefined) {
+										res.status(400).json({ message: 'userbooks: please provide book' });
+									} else {
+										res.status(201).json(added)
+									}
+								})
+								.catch(err => {
+									res.status(500).json({ message: 'error in posting userbook' });
+								})
+						})
+						.catch(err =>
+							res.status(500).json({ message: 'Book not added' })
+						);
+				} else {
+					res.status(200).json(bk);
+				}
+			})
+			.catch(err => {
+				res.status(500).json({
+					message: 'Error, something went wrong'
+				});
+			});
+	} else {
+		res.status(400).json({ message: 'Please provide a book' });
+	}
+})
 
 router.get('/:userId/library/:id', (req, res) => {
 	const userId = req.params.userId;
