d3f17ff66332e1f16650bf7b8f9272ac17d1559d
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-02-25 08:52:23 -0500

    configured restrictive middleware for sessions

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,19 +1,7 @@
-const jwt = require('jsonwebtoken');
-
-const { jwtsecret } = require('../config/secrets.js');
-
-module.exports = (req, res, next) => {
-	const token = req.headers.authorization;
-	if (token) {
-		jwt.verify(token, jwtsecret, (err, decodedToken) => {
-			if (err) {
-				res.status(401).json({ message: 'Token is not valid' });
-			} else {
-				//req.email = decodedToken.email
-				next();
-			}
-		});
+module.exports = (request, response, next) => {
+	if (request.session && request.session.user) {
+		next();
 	} else {
-		res.status(401).json({ message: 'Authorization please' });
+		response.status(401).json({ message: 'unauthorized' });
 	}
 };
