fbffc899d71df517b769da08d5ccc58eb6c46de0
Aasa-Christian <aasachristian@yahoo.com>
2020-02-25 13:45:21 -0500

    changed email to emailAddress in users.js seed

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -2,8 +2,8 @@ const faker = require("faker");
 
 
 const createFakeUser = () => ({
-	fullName: `${faker.name.firstName()} ${faker.name.lastName()}`
-	email: faker.internet.email(),
+	fullName: `${faker.name.firstName()} ${faker.name.lastName()}`,
+	emailAddress: faker.internet.email(),
 	username: `${faker.internet.userName()}-${Date.now()}`,
 	password: faker.internet.password()
 });
