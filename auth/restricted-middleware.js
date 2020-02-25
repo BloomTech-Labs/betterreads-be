45076e1f8de8f5d9239c173fffb54f0d8b35cb64
Miguel <miguelqnicolas@gmail.com>
2020-02-25 11:42:36 -0500

    resolving merge conflicts

--- auth/restricted-middleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (request, response, next) => {
-	if (request.session && request.session.user) {
-		next();
-	} else {
-		response.status(401).json({ message: 'unauthorized' });
-	}
-};
--- index.js
+++
@@ -1,5 +0,0 @@
-const server = require('./api/server.js');
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => console.log(`server listening on port ${port}`));
