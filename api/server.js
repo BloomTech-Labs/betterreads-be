5434a3774c7e8d4bb8b26eefead1e8085b043beb
Aasa-Christian <aasachristian@yahoo.com>
2020-02-25 17:02:04 -0500

    added user-shelves router

--- api/server.js
+++ api/server.js
@@ -15,6 +15,7 @@ const restricted = require("../auth/restricted-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const booksRouter = require("../routers/book-router.js");
 const userBooksRouter = require("../routers/user-books-router.js");
+const userShelvesRouter = require("../routers/user-shelves-router")
 
 // MARK: -- server
 const server = express();
@@ -50,6 +51,7 @@ server.use(passport.session());
 server.use("/api/auth", authRouter);
 server.use("/api/books", restricted, booksRouter);
 server.use("/api", restricted, userBooksRouter);
+server.use("/api/shelves", restricted, userShelvesRouter);
 
 server.get("/", (request, response) =>
 	response.status(200).json({ message: "server is working" })
