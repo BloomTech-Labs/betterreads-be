8c771d02820b2803444a2e82d87753c2f082159b
Aasa-Christian <aasachristian@yahoo.com>
2020-02-27 14:03:40 -0500

    reordered the helper functions in user-books-router.js

--- routers/user-books-router.js
+++ routers/user-books-router.js
@@ -17,6 +17,88 @@ router.get("/:userId/library", (req, res) => {
 		);
 });
 
+router.get("/:userId/library/:id", (req, res) => {
+	const userId = req.params.userId;
+	const bookId = req.params.id;
+	UserBooks.findDetailByUserId(userId, bookId)
+		.then(userbook => {
+			if (userbook == undefined) {
+				res.status(400).json({ message: "userbook: does not exist" });
+			} else {
+				res.status(200).json(userbook);
+			}
+		})
+		.catch(err =>
+			res.status(500).json({ message: "error in returning data" })
+		);
+});
+
+router.put("/:userId/library/:id", (req, res) => {
+	const userId = req.params.userId;
+	const bookId = req.params.id;
+	const rs = req.body.readingStatus;
+	UserBooks.updateReadingStatus(userId, bookId, rs)
+		.then(userbook => {
+			if (userbook == undefined) {
+				res.status(400).json({
+					message: "userbook: does not exist. no change."
+				});
+			} else {
+				res.status(201).json(userbook);
+			}
+		})
+		.catch(err =>
+			res.status(500).json({ message: "error in changing data" })
+		);
+});
+
+// MARK: -- Delete userbook from library grab id through body
+router.delete("/:userId/library/", (req, res) => {
+	const userId = req.params.userId;
+	const bookId = req.body.id;
+	UserBooks.remove(userId, bookId)
+		.then(deleted => {
+			if (deleted == undefined) {
+				res.status(400).json({
+					message: "userbook: does not exist. nothing removed."
+				});
+			} else {
+				if (deleted == 0) {
+					res.status(500).json({
+						message: "deleted == 0, nothing was deleted"
+					});
+				} else {
+					res.status(204).json(deleted);
+				}
+			}
+		})
+		.catch(err =>
+			res.status(500).json({ message: "error in removing data" })
+		);
+});
+
+router.delete("/:userId/library/:id", (req, res) => {
+	const userId = req.params.userId;
+	const bookId = req.params.id;
+	UserBooks.remove(userId, bookId)
+		.then(deleted => {
+			if (deleted == undefined) {
+				res.status(400).json({
+					message: "userbook: does not exist. nothing removed."
+				});
+			} else {
+				if (deleted == 0) {
+					res.status(500).json({ message: "userbook: not deleted" });
+				} else {
+					res.status(204).json(deleted);
+				}
+			}
+		})
+		.catch(err =>
+			res.status(500).json({ message: "error in removing data" })
+		);
+});
+
 // MARK: -- REFACTOR, I WILL BREAK THIS DOWN
 router.post("/:userId/library", (req, res) => {
 	const userId = req.params.userId;
@@ -106,87 +188,4 @@ router.post("/:userId/library", (req, res) => {
 		res.status(400).json({ message: "Please provide a book" });
 	}
 });
-
-router.get("/:userId/library/:id", (req, res) => {
-	const userId = req.params.userId;
-	const bookId = req.params.id;
-	UserBooks.findDetailByUserId(userId, bookId)
-		.then(userbook => {
-			if (userbook == undefined) {
-				res.status(400).json({ message: "userbook: does not exist" });
-			} else {
-				res.status(200).json(userbook);
-			}
-		})
-		.catch(err =>
-			res.status(500).json({ message: "error in returning data" })
-		);
-});
-
-router.put("/:userId/library/:id", (req, res) => {
-	const userId = req.params.userId;
-	const bookId = req.params.id;
-	const rs = req.body.readingStatus;
-	UserBooks.updateReadingStatus(userId, bookId, rs)
-		.then(userbook => {
-			if (userbook == undefined) {
-				res.status(400).json({
-					message: "userbook: does not exist. no change."
-				});
-			} else {
-				res.status(201).json(userbook);
-			}
-		})
-		.catch(err =>
-			res.status(500).json({ message: "error in changing data" })
-		);
-});
-
-// MARK: -- Delete userbook from library grab id through body
-router.delete("/:userId/library/", (req, res) => {
-	const userId = req.params.userId;
-	const bookId = req.body.id;
-	UserBooks.remove(userId, bookId)
-		.then(deleted => {
-			if (deleted == undefined) {
-				res.status(400).json({
-					message: "userbook: does not exist. nothing removed."
-				});
-			} else {
-				if (deleted == 0) {
-					res.status(500).json({
-						message: "deleted == 0, nothing was deleted"
-					});
-				} else {
-					res.status(204).json(deleted);
-				}
-			}
-		})
-		.catch(err =>
-			res.status(500).json({ message: "error in removing data" })
-		);
-});
-
-router.delete("/:userId/library/:id", (req, res) => {
-	const userId = req.params.userId;
-	const bookId = req.params.id;
-	UserBooks.remove(userId, bookId)
-		.then(deleted => {
-			if (deleted == undefined) {
-				res.status(400).json({
-					message: "userbook: does not exist. nothing removed."
-				});
-			} else {
-				if (deleted == 0) {
-					res.status(500).json({ message: "userbook: not deleted" });
-				} else {
-					res.status(204).json(deleted);
-				}
-			}
-		})
-		.catch(err =>
-			res.status(500).json({ message: "error in removing data" })
-		);
-});
-
 module.exports = router;
