19b40f5593bf5e7946b117b625e965b2769fb0b0
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-27 10:57:27 -0800

    removed knex-cleaner from db-config, modified seeds for user-shelves, deleted console.logs in all routers and pretified routers

--- routers/user-genre-router.js
+++ routers/user-genre-router.js
@@ -1,27 +1,19 @@
 const router = require("express").Router();
 const Genre = require("../models/user-genre");
 
-
-
-
-
 router.get("/:id", (req, res) => {
-	const userId = req.params.id;
+    const userId = req.params.id;
     Genre.findByUserId(userId)
         .then(userGenre => {
-            console.log(" userGenre ",userGenre)
-			if (!userGenre[0]) {
-				res.status(400).json({ message: "Genre not found for User" });
-			} else {
-				res.status(200).json(userGenre[0]);
-			}
-		})
-		.catch(err =>{
-            res.status(500).json({ message: "error in returning data" })
-            console.log(err) 
-        }
-            
-		);
+            if (!userGenre[0]) {
+                res.status(400).json({ message: "Genre not found for User" });
+            } else {
+                res.status(200).json(userGenre[0]);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "error in returning data" });
+        });
 });
 
 router.post("/", (req, res) => {
@@ -30,70 +22,74 @@ router.post("/", (req, res) => {
     const userGenreObj = {
         userId: userId,
         genreName: genreName
-    }
-    Genre.findByUserId(userId)
-    .then(genre => {
-        console.log("genre", genre, genre.length)
-        if (!genre[0]){
+    };
+    Genre.findByUserId(userId).then(genre => {
+        if (!genre[0]) {
             Genre.add(userGenreObj)
-            .then(userGenre => {
-                res.status(201).json(userGenre)
-                console.log("userGenre",userGenre);
-            })
-            .catch(err => {
-                res.status(500).json({message: "failed to add user genre"}); 
-                console.log(err)
-            })
+                .then(userGenre => {
+                    res.status(201).json(userGenre);
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        message: "failed to add user genre"
+                    });
+                });
         } else {
-                res.status(500).json({ message: "userGenre Tbl already exist make Put request to update existing Tbl" })
+            res.status(500).json({
+                message:
+                    "userGenre Tbl already exist make Put request to update existing Tbl"
+            });
         }
-    } )
+    });
 });
 
 router.put("/", (req, res) => {
     const userId = req.body.userId;
     const updatedGenreName = req.body;
-    
-    Genre.findByUserId(userId)
-    .then(genre => {
+
+    Genre.findByUserId(userId).then(genre => {
         const genreUserId = genre[0].userId;
 
-        if (genreUserId){
+        if (genreUserId) {
             Genre.update(updatedGenreName, genreUserId)
-            .then(userGenre => {
-                res.status(201).json({message: "User genre Update Successful", userGenreId: userGenre})
-                
-            })
-            .catch(err => {
-                res.status(500).json({message: "failed to update user genre"}); 
-                 console.log(err)
-            })
+                .then(userGenre => {
+                    res.status(201).json({
+                        message: "User genre Update Successful",
+                        userGenreId: userGenre
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        message: "failed to update user genre"
+                    });
+                });
         } else {
-                res.status(500).json({ message: "userGenre not found" })
+            res.status(500).json({ message: "userGenre not found" });
         }
-    } )
+    });
 });
 
 router.delete("/:userId", (req, res) => {
     const userId = req.params.userId;
-   
-    Genre.findByUserId(userId)
-    .then(genre => {
-        
-        if (genre){
+
+    Genre.findByUserId(userId).then(genre => {
+        if (genre) {
             Genre.remove(userId)
-            .then(userGenre => {
-                res.status(201).json({message: "User genre Deleted", userGenreId: userGenre})
-                
-            })
-            .catch(err => {
-                res.status(500).json({message: "failed to delete user genre"}); 
-                 console.log(err)
-            })
+                .then(userGenre => {
+                    res.status(201).json({
+                        message: "User genre Deleted",
+                        userGenreId: userGenre
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        message: "failed to delete user genre"
+                    });
+                });
         } else {
-                res.status(500).json({ message: "userGenre not found" })
+            res.status(500).json({ message: "userGenre not found" });
         }
-    } )
+    });
 });
 
 module.exports = router;
