65564e4a779fc5e30b77461ece8e9e68c251e997
Aasa-Christian <aasachristian@yahoo.com>
2020-02-27 11:59:13 -0500

    updated user-genre router and model

---
+++ models/user-genre.js
@@ -0,0 +1,41 @@
+const db = require("../database/db-config.js");
+
+module.exports = {
+	
+    findByUserId,
+    findById,
+	add,
+	update,
+	remove
+};
+
+
+
+function findByUserId(userId) {
+	return db("userGenre")
+	.where( "userGenre.userId", userId );
+}
+
+
+function findById(id) {
+	return db("userGenre").where({ id });
+}
+async function add(genre) {
+	const [id] = await db("userGenre")
+		.insert(genre)
+		.returning("id");
+	return findById(id);
+}
+
+
+  function update(updatedGenre, userId) {
+	return db("userGenre")
+	.update(updatedGenre)
+	.where("userId", userId)
+}
+
+function remove(userId) {
+	return db("userGenre")
+        .where("userId", userId)
+		.del();
+}
