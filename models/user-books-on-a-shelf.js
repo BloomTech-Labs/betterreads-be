bd6126818f28d4f8a88fb406180b283d8087841d
Nicole Hollis <nhollis@gmail.com>
2020-02-27 15:08:29 -0800

    Completed router and model, all endpoints working

--- models/user-books-on-a-shelf.js
+++ models/user-books-on-a-shelf.js
@@ -1,17 +1,39 @@
 const db = require("../database/db-config.js");
 
 module.exports = {
-findBy,
-add
+	findBooksOnShelf,
+	addBooks,
+	remove
 };
 
-function findBy(filter) {
-	return db("userBooksOnAShelf").where(filter);
+function findBooksOnShelf(shelfId) {
+	return db('userBooksOnAShelf as bs')
+		.join('books as b', 'bs.bookId', 'b.id')
+		.join('userShelves as s', 's.id', 'bs.shelfId')
+		.where('shelfId', shelfId)
+		.select('b.id', 'b.title','s.shelfName', 's.userId' )
+}
+// function findBy(filter) {
+// 	return db("userBooksOnAShelf").where(filter);
+// }
+
+function findById(id) {
+	return db('userBooksOnAShelf')
+		.where ({ id }).first();
 }
 
-async function add(book) {
-	const [id] = await db("userShelves")
+
+async function addBooks(book) {
+	const [id] = await db("userBooksOnAShelf")
 		.insert(book)
 		.returning("id");
 	return findById(id);
+}
+
+
+
+async function remove(bookId) {
+	return db("userBooksOnAShelf")
+		.where("bookId", bookId)
+		.del();
 }
