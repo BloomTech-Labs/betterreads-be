55861bd4baf67f11f6476d1d8483ade1542d4198
Aasa-Christian <aasachristian@yahoo.com>
2020-02-28 16:55:56 -0500

    updated user shelves test

--- test/user-shelves-router.test.js
+++ test/user-shelves-router.test.js
@@ -13,16 +13,16 @@ var options = {
 
 describe("user-shelves-router", function() {
 	const Test1 = {
-        "shelfName": " Test Shelf 1",
-        "isPrivate": false
+        shelfName: " Test Shelf 1",
+        isPrivate: false
     }
     const Test2 = {
-        "shelfName": " Test Shelf 2",
-        "isPrivate": false
+        shelfName: " Test Shelf 2",
+        isPrivate: false
     }
     const Test3 = {
-        "shelfName": " Test Shelf 3",
-        "isPrivate": false
+        shelfName: " Test Shelf 3",
+        isPrivate: false
     }
 
     
@@ -59,14 +59,63 @@ describe("user-shelves-router", function() {
 			})
     });
     
-    describe("GET user shelves user Id", function() {
-		it("GET user.Id", function() {
+    describe("POST user shelves user Id", function() {
+		it("POST res is user.Id", function() {
 			return promisedCookie({ emailAddress: "seedemail", password: "seedpassword" }).then(cookie => {
 				const req = request(server)
-					.get("/api/shelves/user/1")
+					.post("/api/shelves/1")
+					.send(Test1)
 					.set("cookie", cookie)
 					.then(res => {
 						expect(res.body[0].userId).toBe(1);
+					});
+				return req;
+			});
+        });
+        
+	});
+	
+	describe("POST user shelves", function() {
+		it("POST res shelfName toBe Test Shelf 1", function() {
+			return promisedCookie({ emailAddress: "seedemail", password: "seedpassword" }).then(cookie => {
+				const req = request(server)
+					.post("/api/shelves/1")
+					.send(Test1)
+					.set("cookie", cookie)
+					.then(res => {
+						expect(res.body[0].shelfName).toBe(" Test Shelf 1");
+					});
+				return req;
+			});
+        });
+        
+	});
+	
+	describe("PUT user shelves", function() {
+		it("PUT res message is userShelf: does not exist", function() {
+			return promisedCookie({ emailAddress: "seedemail", password: "seedpassword" }).then(cookie => {
+				const req = request(server)
+					.put("/api/shelves/1000")
+					.send(Test1)
+					.set("cookie", cookie)
+					.then(res => {
+						expect(res.body.message).toBe( "userShelf: does not exist");
+					});
+				return req;
+			});
+        });
+        
+	});
+	
+	describe("PUT user shelves", function() {
+		it("PUT res.status toBe 200", function() {
+			return promisedCookie({ emailAddress: "seedemail", password: "seedpassword" }).then(cookie => {
+				const req = request(server)
+					.put("/api/shelves/1")
+					.send(Test2)
+					.set("cookie", cookie)
+					.then(res => {
+						expect(res.status).toBe(200);
 					});
 				return req;
 			});
