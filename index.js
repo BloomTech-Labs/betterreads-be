b6c04e353d90b46e6e1617259b1345b6072aa17e
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-03-04 08:48:07 -0800

    changed index port on index.js and changed start script on package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8081;
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
