bcf165ab05d3f943ab22693eed86939f7b7682fe
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-03-12 10:29:12 -0700

    added cors

--- api/server.js
+++ api/server.js
@@ -30,7 +30,7 @@ server.use(helmet());
 // MARK: -- cors
 // const whitelist = ["http://localhost:3000", "https://readrr.app", "*"]
 var corsOptions = {
-	origin: "https://www.api.readrr.app",
+	origin: "https://www.readrr.app",
 	methods: "GET,PUT,POST,DELETE",
    	credentials: true
 }
