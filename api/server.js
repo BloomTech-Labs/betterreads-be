4c77395973c5e179844147996974011fda613f9d
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-03-11 17:21:19 -0700

    fixed issue on update

--- api/server.js
+++ api/server.js
@@ -30,7 +30,7 @@ server.use(helmet());
 // MARK: -- cors
 // const whitelist = ["http://localhost:3000", "https://readrr.app", "*"]
 var corsOptions = {
-	origin: "https://www.readrr.app",
+	origin: "http://localhost:3000",
 	methods: "GET,PUT,POST,DELETE",
    	credentials: true
 }
--- routers/user-books-router.js
+++ routers/user-books-router.js
@@ -60,6 +60,7 @@ router.put("/:userId/library", (req, res) => {
       res.status(201).json(updated)
     })
     .catch(err => {
+      console.log(err)
       res.status(500).json(err)
     })
 });
