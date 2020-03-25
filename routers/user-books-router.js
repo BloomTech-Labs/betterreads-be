5629a602d81ab1812f7176020951af0ebf5151cc
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-03-25 08:05:22 -0700

    updating tests for coverage

--- routers/user-books-router.js
+++ routers/user-books-router.js
@@ -53,17 +53,13 @@ router.put("/:userId/library", (req, res) => {
       dateEnded: dateEnded,
       userRating: rating 
     }
-  )
-    .then(updated => {
+  ).then(updated => {
       if(updated == undefined) { 
         res.status(400).json({ message: "cannot update book, not found in library" })
       } else {
         res.status(201).json(updated)
       }
-    })
-    .catch(err => {
-      res.status(500).json(err)
-    })
+    }).catch(err => res.status(500).json(err))
 });
 
 // MARK: -- DELETE
@@ -71,33 +67,23 @@ router.put("/:userId/library", (req, res) => {
 router.delete("/:userId/library", (req, res) => {
   const userId = req.params.userId;
   const bookId = req.body.bookId;
-  UserBooks.remove(userId, bookId)
-    .then(deleted => {
-      if (deleted == undefined) {
-        res.status(400).json({
-          message: "userbook: does not exist. nothing removed."
-        });
+  UserBooks.remove(userId, bookId).then(deleted => {
+    if (deleted == undefined) {
+      res.status(400).json({ message: "userbook: does not exist. nothing removed." });
+    } else {
+      if (deleted == 0) {
+        res.status(500).json({ message: "deleted == 0, nothing was deleted" });
       } else {
-        if (deleted == 0) {
-          res.status(500).json({
-            message: "deleted == 0, nothing was deleted"
-          });
-        } else {
-          BooksOnAShelf.removeAll(bookId, userId)
-          .then(removed => {
-            if (removed.length > 0){
-              res.status(204).json({message: "book deleted from user shelf and library"})
-            } else {
-              res.status(204).json({message: "book deleted from user library"})
-            }
-          })
-          .catch(err => {
-            res.status(404).json({message: "error removing book from shelf"})
-          })
-        }
-      }
-    })
-    .catch(err => res.status(500).json({ message: "error in removing data" }));
+        BooksOnAShelf.removeAll(bookId, userId).then(removed => {
+          if (removed.length > 0){
+            res.status(204).json({message: "book deleted from user shelf and library"})
+          } else {
+            res.status(204).json({message: "book deleted from user library"})
+          }
+        }).catch(err => res.status(404).json({message: "error removing book from shelf"}))
+      } 
+    }
+    }).catch(err => res.status(500).json({ message: "error in removing data" }));
 });
 
 // MARK: -- POST
@@ -111,43 +97,29 @@ router.post("/:userId/library", (req, res) => {
   if (book) {
     const googleId = book.googleId;
     // MARK: -- is the book in the user's library already?
-    UserBooks.isBookInUserBooks(userId, googleId)
-      .then(library => {
+    UserBooks.isBookInUserBooks(userId, googleId).then(library => {
         // MARK: -- length == 0, user does not have book in their library
         if (library.length == 0) {
           // MARK: -- check to see if the book in our books database
-          Books.findBy({ googleId })
-            .first()
-            .then(bk => {
-              if (bk == undefined) {
-                // MARK: -- adding the book to our books db since it is not there
-                Books.add(book)
-                  .then(book => {
-                    const newUserBookObject = helper.createUserBook(book, userId, favorite, status,rating)
-                    // MARK: -- adding book to our user's library
-                    helper.addToUserBooks(req, res, UserBooks, newUserBookObject)
-                  })
-                  .catch(err => {
-                    res.status(500).json({
-                      message: "Book not added to book db"
-                    });
-                  });
-              } else {
-                const userBookObject = helper.createUserBook(bk, userId, favorite, status, rating)
-                // MARK: -- book exist in our books db, add the book to our user's library
-                helper.addToUserBooks(req, res, UserBooks, userBookObject)
-              }
-            });
+          Books.findBy({ googleId }).first().then(bk => {
+            if (bk == undefined) {
+              // MARK: -- adding the book to our books db since it is not there
+              Books.add(book).then(book => {
+                const newUserBookObject = helper.createUserBook(book, userId, favorite, status,rating)
+                // MARK: -- adding book to our user's library
+                helper.addToUserBooks(req, res, UserBooks, newUserBookObject)
+              }).catch(err => res.status(500).json({ message: "Book not added to book db" }));
+            } else {
+              const userBookObject = helper.createUserBook(bk, userId, favorite, status, rating)
+              // MARK: -- book exist in our books db, add the book to our user's library
+              helper.addToUserBooks(req, res, UserBooks, userBookObject)
+            }
+          });
         } else {
           // MARK: -- user already has the book in their user library
-          res.status(200).json({
-            message: "Book already exist in your library"
-          });
+          res.status(200).json({ message: "Book already exist in your library" });
         }
-      })
-      .catch(nothere => {
-        res.status(500).json({ message: "Error in userbook posting" });
-      });
+      }).catch(nothere => res.status(500).json({ message: "Error in userbook posting" }));
   } else {
     // MARK: -- book did not have information provided
     res.status(400).json({ message: "Please provide a book" });
