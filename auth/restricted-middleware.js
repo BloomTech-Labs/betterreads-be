29847eddb1342170971dcc82fc586c5f6554501b
darkwolfxj <hesitation.dotdot@gmail.com>
2020-04-16 13:49:02 -0400

    updated genre endpoint to work with more than one genre, reworked authorization with JSON web tokens

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,7 +1,11 @@
+const secret = require("../config/secrets").jwtSecret
+const jwt = require("jsonwebtoken")
+
 module.exports = (request, response, next) => {
-	if (request.session && request.session.user) {
-		next();
-	} else {
-		response.status(401).json({ message: "unauthorized" });
-	}
+    const token = request.headers.authorization
+    if (token && jwt.verify(token, secret)){
+        next()
+    } else {
+        response.status(401).json({ message: "log in, first" })
+    }
 };
