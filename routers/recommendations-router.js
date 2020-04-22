3b9b9260a981239fb84eb465d1fffee7bf70e704
darkwolfxj <hesitation.dotdot@gmail.com>
2020-04-22 19:51:06 -0400

    updated recommendations endpoint

--- routers/recommendations-router.js
+++ routers/recommendations-router.js
@@ -6,7 +6,7 @@ router.get("/:userId/recommendations", (req, res) => {
    const userId = req.params.userId;
    UserBooks.findByUserId(userId)
         .then(books => {
-            axios.post("http://staging-env.eba-mydnqmmk.us-east-1.elasticbeanstalk.com/recommendations", books)
+            axios.post("http://betterreads-api.us-east-1.elasticbeanstalk.com/recommendations", books)
             .then(recs => res.status(200).json({ message: "recommendations retrieved successfully", recommendations: recs.data }))
             .catch(({ name, message, stack }) => res.status(500).json({ error: "error retrieving recommendations", name, message, stack }))})
         .catch(({ name, message, stack }) => res.status(404).json({ error: "could not find a user by the requested id", name, message, stack }))            
--- test/book-router.spec.js
+++ test/book-router.spec.js
@@ -24,56 +24,58 @@ const randomPageNum = async () => {
     return float;
 }
 
-{(describe("GET to /api/books", () => {
+describe("book-router.js", () => {
+    
+describe("GET to /api/books", () => {
 
-    it("returns 200 ok and an array of books", async () => {
-        
-        const response = await request(server).get("/api/books").set({ authorization: `${ await getToken() }` });
-        
-        expect(response.body).not.toBe(undefined);
-        expect(response.status).toBe(200);
-        
+        it("returns 200 ok and an array of books", async () => {
+            
+            const response = await request(server).get("/api/books").set({ authorization: `${ await getToken() }` });
+            
+            expect(response.body).not.toBe(undefined);
+            expect(response.status).toBe(200);
+            
+        });
+    });
+
+    describe("GET to /api/books/:bookId", () => {
+        it("returns status 200 and a single book", async () => {
+            const response = await request(server).get("/api/books/1").set({ authorization: `${ await getToken() }` });
+            
+            expect(response.body).not.toBe(undefined);
+            expect(response.body.id).not.toBe(undefined);
+            expect(response.body.id).toBe(1);
+        });
+    });
+
+    describe("POST to /api/books", () => {
+        afterEach(async () => {
+            await books.del({ authors: "test" });
+        })
+        it("returns status 201 and a message", async () => {
+            const response = await request(server).post("/api/books").set({ authorization: `${ await getToken() }`})
+                .send({ 
+                    "googleId": `-99.${ await randomGoogleId() }`,
+                    "title": `test${ await randomTitle() }`,
+                    "authors": "test",
+                    "publisher": "test",
+                    "publishedDate": "test",
+                    "description": "test",
+                    "isbn10": "test",
+                    "isbn13": "test",
+                    "pageCount": await randomPageNum(),
+                    "categories": "test",
+                    "thumbnail": "test",
+                    "smallThumbnail": "test",
+                    "language": "test",
+                    "webReaderLink": "test",
+                    "textSnippet": "test",
+                    "isEbook": false,
+                    "averageRating": "3.00"
+                });
+            
+                expect(response.body.message).toBe("Added book to our api");
+                expect(response.status).toBe(201);
+        });
     });
 });
-
-describe("GET to /api/books/:bookId", () => {
-    it("returns status 200 and a single book", async () => {
-        const response = await request(server).get("/api/books/1").set({ authorization: `${ await getToken() }` });
-        
-        expect(response.body).not.toBe(undefined);
-        expect(response.body.id).not.toBe(undefined);
-        expect(response.body.id).toBe(1);
-    });
-});
-
-describe("POST to /api/books", () => {
-    afterEach(async () => {
-        await books.del({ authors: "test" });
-    })
-    it("returns status 201 and a message", async () => {
-        const response = await request(server).post("/api/books").set({ authorization: `${ await getToken() }`})
-            .send({ 
-                "googleId": `-99.${ await randomGoogleId() }`,
-                "title": `test${ await randomTitle() }`,
-                "authors": "test",
-                "publisher": "test",
-                "publishedDate": "test",
-                "description": "test",
-                "isbn10": "test",
-                "isbn13": "test",
-                "pageCount": await randomPageNum(),
-                "categories": "test",
-                "thumbnail": "test",
-                "smallThumbnail": "test",
-                "language": "test",
-                "webReaderLink": "test",
-                "textSnippet": "test",
-                "isEbook": false,
-                "averageRating": "3.00"
-            });
-        
-            expect(response.body.message).toBe("Added book to our api");
-            expect(response.status).toBe(201);
-    });
-});
-)}
