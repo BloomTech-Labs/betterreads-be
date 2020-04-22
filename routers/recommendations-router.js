432f5342141ea4f417f85cab70f515e926b3840b
darkwolfxj <hesitation.dotdot@gmail.com>
2020-04-22 18:26:48 -0400

    made some changes requested by FE and merged feature/testing in

--- routers/recommendations-router.js
+++ routers/recommendations-router.js
@@ -7,20 +7,7 @@ router.get("/:userId/recommendations", (req, res) => {
    UserBooks.findByUserId(userId)
         .then(books => {
             axios.post("http://staging-env.eba-mydnqmmk.us-east-1.elasticbeanstalk.com/recommendations", books)
-            .then(recs => {
-                const recIds = recs.data.recommendations.map(rec => rec.id);
-                const convertedIds = () => {
-                    const convertedData = []
-                    for(let i = 0; i < recIds.length; i++){
-                        let obj = {};
-                        const newObj = { ...obj, googleId: `${ recIds[i] }` }
-                        convertedData.push(newObj)
-                    }
-                    return convertedData
-                }
-                
-                res.status(200).json({ message: "recommendations retrieved successfully", recommendations: convertedIds() })
-            })
+            .then(recs => res.status(200).json({ message: "recommendations retrieved successfully", recommendations: recs.data }))
             .catch(({ name, message, stack }) => res.status(500).json({ error: "error retrieving recommendations", name, message, stack }))})
         .catch(({ name, message, stack }) => res.status(404).json({ error: "could not find a user by the requested id", name, message, stack }))            
 })
